ab8d06f2b79d25ae2407b1edba060b739ee89aeb
sahvsergio <sergioandresherrera@gmail.com>
2025-02-09 10:52:24 -0500

    add structure for Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,3 +1,41 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+
+class Game {
+  constructor() {
+    this.missed=0;
+    this.phrases=
+    [
+        Phrase(),
+        Phrase(),
+        Phrase(),
+        Phrase(),
+        Phrase()
+      ]
+    this.activePhrase=null;  
+   
+  }
+  startGame(){
+
+  }
+
+  getRandomPhrase(){
+
+
+  }
+  handleInteraction(){
+
+
+  }
+  removeLife(){
+
+  }
+  checkForWin(){
+
+  }
+gameOver(){
+
+  
+}
+}
