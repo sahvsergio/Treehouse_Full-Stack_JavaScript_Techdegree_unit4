615a9c46a4be7147604fc4617e1cbb0c0126b092
sahvsergio <sergioandresherrera@gmail.com>
2025-02-11 21:21:40 -0500

    added prhase to the dom without showing the letters on the screen

--- js/Game.js
+++ js/Game.js
@@ -6,19 +6,11 @@ class Game {
   constructor() {
     this.missed = 0;
     this.phrases = [
-      new Phrase(
-        "The waste of life occasioned by trying to do too many things at once is appalling. No one is large enough to be split up into many parts; and the sooner a man can stamp this truth upon his mind, the better his chances for being a profitable member of society."
-      ),
-      new Phrase(
-        "That man is the richest whose pleasures are the cheapest. ~Henry David Thoreau"
-      ),
-      new Phrase("If at first you do succeed, try something harder."),
-      new Phrase(
-        "A man of talent has a good Working Consciousness, a man of genius a good Working Sub-Consciousness. Hence the frequent mental instability of genius"
-      ),
-      new Phrase(
-        "I have known no man of genius who had not to pay, in some affliction or defect either physical or spiritual, for what the gods had given him."
-      ),
+      new Phrase("The years teach much which the days never know."),
+      new Phrase("A true friend is for ever a friend"),
+      new Phrase("Hold a true friend with both your hands"),
+      new Phrase("To good eating belongs good drinking"),
+      new Phrase("Friendship is a sheltering tree"),
     ];
 
 
@@ -27,21 +19,31 @@ class Game {
 
   startGame() {
 
-    /*hides the start screen overlay, 
-    calls the getRandomPhrase() method, and sets the
-     activePhrase property with the chosen phrase.
-      It also adds that phrase to the board by calling
-       the addPhraseToDisplay() method on the activePhrase property.*/
+    /*
+    
+      It also*/
+
+  //hides the start screen overlay     
   let overlay=document.querySelector('#overlay');
   overlay.style.display='none';
+  //calls the getRandomPhrase() method, and sets the 
+  //activePhrase property with the chosen phrase.
+  this.activePhrase=this.getRandomPhrase()
+   //adds that phrase to the board by calling
+  //the addPhraseToDisplay() method on the activePhrase property.
+  this.activePhrase.addPhraseToDisplay();
+  
+
+  
+ 
 
   }
 
   getRandomPhrase() {
-    console.log(this.phrases);
+    let randomizedIndex = Math.ceil(Math.random()*(this.phrases.length)-1);
     
+    return this.phrases[randomizedIndex];
    
-
   }
   handleInteraction() {}
   removeLife() {}
--- js/Phrase.js
+++ js/Phrase.js
@@ -6,21 +6,56 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
   }
-  addPhraseToDisplay(){
-    /* adds letter placeholders to the display 
-    when the game starts. 
-    Each letter is presented by an empty box,
-     one li element for each letter. See the  
-    */
-     let phraseUL=document.querySelector('#phrase>ul');
+  addPhraseToDisplay(phrase) {
+  
+    let splitPhrase = this.phrase.split('');
+    let liHtml='';
+    let phraseUL=document.querySelector("#phrase>ul")
+    console.log(splitPhrase);
+    console.log(splitPhrase.length);
+    for(let i=0;i<splitPhrase.length;i++){
+      if (splitPhrase[i]==''){
+        console.log(splitPhrase[i]);
+      
+      
+    }
+    else{
+      liHtml += ` <li class="hide letter ${splitPhrase[i]}"></li> `;
      
 
-  }
-  checkLetter(){
-
-  }
-  showMatchedLetter(){
+    }
     
   }
+  phraseUL.innerHTML=liHtml;
 }
+   
+    
+       
+
+
+  checkLetter() {}
+  showMatchedLetter() {}
+}
+   
+    
+
+    
+    
+    
+
+    
+
+
+
+    
+    //Each letter is presented by an empty box,
+     //one li element for each letter. See the  
+    
+     
+     
+  
+    
+
+ 
+
 
